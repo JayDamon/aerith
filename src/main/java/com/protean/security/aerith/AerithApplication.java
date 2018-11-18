36daee3ca1d4d0e95c1521685c57c38f08f94924
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2018-11-18 16:13:33 -0500

    Initial commit

---
+++ src/main/java/com/protean/security/aerith/AerithApplication.java
@@ -0,0 +1,12 @@
+package com.protean.security.aerith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AerithApplication.class, args);
+    }
+}
